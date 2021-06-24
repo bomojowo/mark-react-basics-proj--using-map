a4882fb0e0a74dcb8e9ab34a2746be2a58c582ab
Bukola Omojowo <bomojowo@gmail.com>
2021-06-24 14:44:33 +0100

    Commented out some code

--- src/vanilla/0-examples/0-double-numbers.ts
+++ src/vanilla/0-examples/0-double-numbers.ts
@@ -1,16 +1,16 @@
-function doubleNumbers(numbers: number[]): number[] {
-  return numbers.map(doubleNumber);
-}
-
-function doubleNumber(n: number): number {
-  return n * 2;
-}
-
-// const doubleNumber = (n: number): number => n * 2;
-
 // function doubleNumbers(numbers: number[]): number[] {
-//   return numbers.map((n: number) => n * 2);
-//   // return numbers.map((n) => n * 2)
+//   return numbers.map(doubleNumber);
 // }
+
+// function doubleNumber(n: number): number {
+//   return n * 2;
+// }
+
+const doubleNumber = (n: number): number => n * 2;
+
+function doubleNumbers(numbers: number[]): number[] {
+  return numbers.map((n: number) => n * 2);
+  // return numbers.map((n) => n * 2)
+}
 
 export { doubleNumbers, doubleNumber };
