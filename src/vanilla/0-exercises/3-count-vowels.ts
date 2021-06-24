1b1d1cff9855d2690d0b19cd9d95bd182fe1e201
Bukola Omojowo <bomojowo@gmail.com>
2021-06-24 12:45:19 +0100

    Wrote code that passes test

--- src/vanilla/0-exercises/3-count-vowels.ts
+++ src/vanilla/0-exercises/3-count-vowels.ts
@@ -1,9 +1,17 @@
-function countVowelsInEach() {
+function countVowelsInEach(words: string[]): number[] {
   // your code here
+  return words.map(countVowels)
 }
 
-function countVowels() {
+function countVowels(word: string): number {
   // your code here
+  let vowels = []
+  for(let char of word){
+    if(['a','e','i','o','u'].includes(char)){
+      vowels.push(char)
+    }
+  }
+  return vowels.length
 }
 
 export { countVowelsInEach, countVowels };
