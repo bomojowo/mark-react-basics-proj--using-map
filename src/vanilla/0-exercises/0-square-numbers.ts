ee62c229d0136c9b735ec9d0d6bc68e48ce16337
Bukola Omojowo <bomojowo@gmail.com>
2021-06-25 12:21:06 +0100

    Used an arrow function for function

--- src/vanilla/0-exercises/0-square-numbers.ts
+++ src/vanilla/0-exercises/0-square-numbers.ts
@@ -1,10 +1,17 @@
-function squareNumbers(numbers: number[]): number[] {
-  // your code here
-  return numbers.map(squareNumber);
-}
+// function squareNumbers(numbers: number[]): number[] {
+//   // your code here
+//   return numbers.map(squareNumber);
+// }
 
-function squareNumber(n: number): number {
-  return n*n;
+// function squareNumber(n: number): number {
+//   return n*n;
+// }
+
+//arrow function
+const squareNumber = (n: number) : number => n * n;
+
+function squareNumbers(numbers: number[]): number[] {
+  return numbers.map(squareNumber)
 }
 
 export { squareNumbers, squareNumber };
