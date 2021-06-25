be5c7ac948990b5cb8f0b2a0006a23a7b3bf26cd
Bukola Omojowo <bomojowo@gmail.com>
2021-06-25 13:26:31 +0100

    Turned function into arrow function

--- src/vanilla/0-exercises/3-count-vowels.ts
+++ src/vanilla/0-exercises/3-count-vowels.ts
@@ -1,10 +1,20 @@
-function countVowelsInEach(words: string[]): number[] {
-  // your code here
-  return words.map(countVowels)
-}
+// function countVowelsInEach(words: string[]): number[] {
+//   // your code here
+//   return words.map(countVowels)
+// }
 
-function countVowels(word: string): number {
-  // your code here
+// function countVowels(word: string): number {
+//   // your code here
+//   let vowels = []
+//   for(let char of word){
+//     if(['a','e','i','o','u'].includes(char)){
+//       vowels.push(char)
+//     }
+//   }
+//   return vowels.length
+// }
+
+const countVowels = (word: string): number => {
   let vowels = []
   for(let char of word){
     if(['a','e','i','o','u'].includes(char)){
@@ -12,6 +22,10 @@ function countVowels(word: string): number {
     }
   }
   return vowels.length
+}
+
+function countVowelsInEach(words: string[]): number[] {
+  return words.map(countVowels);
 }
 
 export { countVowelsInEach, countVowels };
