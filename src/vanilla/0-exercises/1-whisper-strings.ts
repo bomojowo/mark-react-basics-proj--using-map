cfe7ee9a121f0b29293c09828717fe416679f832
Bukola Omojowo <bomojowo@gmail.com>
2021-06-25 13:21:55 +0100

    changed functions into arrow functions

--- src/vanilla/0-exercises/1-whisper-strings.ts
+++ src/vanilla/0-exercises/1-whisper-strings.ts
@@ -1,14 +1,21 @@
+// function whisperStrings(strings: string[]): string[] {
+//   // your code here
+//   return strings.map(whisperString)
+
+// }
+
+// function whisperString(string: string): string {
+//   // your code here
+  
+//   return `shh: ${string.toLowerCase()}`;
+// }
+
+//arrow function
+const whisperString = (string: string): string => `shh: ${string.toLowerCase()}`;
+
 function whisperStrings(strings: string[]): string[] {
-  // your code here
   return strings.map(whisperString)
-
 }
 
-function whisperString(string: string): string {
-  // your code here
-  let lowercaseString = string.toLowerCase();
-
-  return `shh: ${lowercaseString}`;
-}
 
 export { whisperStrings, whisperString };
--- src/vanilla/0-exercises/2-stringify-booleans.ts
+++ src/vanilla/0-exercises/2-stringify-booleans.ts
@@ -1,10 +1,21 @@
-function stringifyBooleans(strings: any[]): string[] {
-  // your code here
-  return strings.map(stringifyBoolean);
-}
+// function stringifyBooleans(strings: any[]): string[] {
+//   // your code here
+//   return strings.map(stringifyBoolean);
+// }
 
-function stringifyBoolean(stringOrBoolean: boolean | string): string {
-  // your code here
+// function stringifyBoolean(stringOrBoolean: boolean | string): string {
+//   // your code here
+//   let makeIntoString = "";
+//   if(typeof stringOrBoolean === "boolean" ){
+//     let booleanToString = stringOrBoolean.toString()
+//     let firstLetterOfBoolean = booleanToString[0].toUpperCase()
+//     return makeIntoString += firstLetterOfBoolean
+//   }
+//   console.log(makeIntoString)
+//   return makeIntoString += stringOrBoolean
+// }
+
+const stringifyBoolean = (stringOrBoolean: boolean | string): string => {
   let makeIntoString = "";
   if(typeof stringOrBoolean === "boolean" ){
     let booleanToString = stringOrBoolean.toString()
@@ -14,5 +25,11 @@ function stringifyBoolean(stringOrBoolean: boolean | string): string {
   console.log(makeIntoString)
   return makeIntoString += stringOrBoolean
 }
+
+function stringifyBooleans(strings: any[]): string[] {
+  return strings.map(stringifyBoolean)
+}
+
+
 
 export { stringifyBooleans, stringifyBoolean };
